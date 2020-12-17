a720b9936bd043989097cb07f51795d44a4c7b8e
Kevin Reece <kreece@salesforce.com>
2020-12-17 14:52:42 +0000

    Refactoring API Requests to separate request, parsing and UI concerns

--- force-app/packaged/main/default/lwc/userInfo/userInfo.js
+++ force-app/packaged/main/default/lwc/userInfo/userInfo.js
@@ -1,17 +1,17 @@
-import { LightningElement, api, track, wire } from 'lwc';
-import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
-import ACCESS_FIELD from '@salesforce/schema/Subscriber_Authentication__c.Access__c';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord, getFieldValue, getRecordNotifyChange } from 'lightning/uiRecordApi';
+import ID from '@salesforce/schema/Subscriber_Authentication__c.Id';
 import fetchUserInfo from '@salesforce/apex/ProvisioningRequestHandler.fetchUserInfo';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class UserInfo extends LightningElement {
     @api recordId;
 
-    @wire(getRecord, { recordId: '$recordId', fields: [ACCESS_FIELD] }) 
+    @wire(getRecord, { recordId: '$recordId', fields: [ ID ] }) 
     record;
 
     handleClick() {
-        fetchUserInfo({ access_token: getFieldValue(this.record.data, ACCESS_FIELD) })
+        fetchUserInfo({ record_id: getFieldValue(this.record.data, ID) })
         .then(result => {
             const evt = new ShowToastEvent({
                 title: 'Authentication Check Response',
